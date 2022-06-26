a1e5fe7520c08ee5807c24bdd77dfe576228e259
Shubham Palav <87655660+shubhampalav2@users.noreply.github.com>
2022-06-26 23:19:28 +0530

    Update Contact.js

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
     <div className="contactContainer">
       <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
-        <Button>Contact: palavshubham09@gmail.com</Button>
+        <Button>Contact: palavshubham123@gmail.com</Button>
       </a>
     </div>
   );
